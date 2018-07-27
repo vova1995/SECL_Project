76e936f562c474567f9335bdf906ce9d5e844118
vova1995 <vovapetryna1995@gmail.com>
2018-07-27 13:20:37 +0300

    delete tags for temporary show_messages

--- netshop/person/templatetags/person.py
+++ netshop/person/templatetags/person.py
@@ -5,10 +5,3 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('netshop/show_messages.html', takes_context=True)
-def show_messages(context, show=True):
-    return {
-                'messages': (context.get('messages') if show else None)
-            }
-
-
