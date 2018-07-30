41aae605df2b46f7fe855e74e35476eddd7376d0
vova1995 <vovapetryna1995@gmail.com>
2018-07-30 20:44:18 +0300

    complete 7 hw and added show_message tag for shop forms

--- netshop/shop/templatetags/shoptags.py
+++ netshop/shop/templatetags/shoptags.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import absolute_import
+import datetime
 from django import template
+from person.models import Person
 
 register = template.Library()
 
@@ -18,3 +20,28 @@ def my_sort(value):
 
 
 register.filter('my_sort', my_sort)
+
+
+
+# @register.filter(expects_localtime=True)
+# def test(value):
+#     return value
+#
+# @register.simple_tag(takes_context=True)
+# def current_time(context, f, prefix=None):
+#     prefix = prefix or context.get('date_prefix', '')
+#     context['t'] = Person.objects.get(pk=1)
+#     return prefix + datetime.datetime.now().strftime(f)
+#
+# @register.inclusion_tag('shop/menu.html', takes_context=True)
+# def menu(context, selected=None):
+#     return {
+#         'items': ['Menu 1', 'Menu 2', 'Menu 3'],
+#         'selected': selected or context.get('selected_menu')
+#     }
+@register.inclusion_tag('shop/show_messages.html', takes_context=True)
+def show_messages(context, message=True):
+    return {
+        'messages': (context.get('messages') if message else None)}
+
+
--- netshop/shop/views.py
+++ netshop/shop/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import random
+
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from shop.models import Shop, TypeShop
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
@@ -39,12 +42,6 @@ class CustomShopDetailView(TemplateView):
 #     model = Shop
 #     fields = ('type_of_shop', 'name', 'owner', 'seller', 'stock', 'city', 'website')
 
-# def get_model_fields(model):
-#     return model._meta.fields
-#
-# for i in get_model_fields(Shop):
-#     if i == 'name':
-#         print(i)
 
 
 def shop_edit(request, pk):
@@ -81,12 +78,18 @@ class TypeCreateView(CreateView):
     model = TypeShop
     fields = ('name',)
 
-# class CustomShopCreateView(TemplateView):
 
 
 def test(request):
     shop = Shop.objects.all()
+    mes = messages.success(request, 'Okey')
     context = {
-        'shop': shop
+        'shop': shop,
+        'test': timezone.now(),
+        'date_prefix': 'date: ',
+        't': 'Vova',
+        'message': mes,
+        'selected_menu': random.randint(0,2)
     }
     return render(request, 'shop/test.html', context)
+
